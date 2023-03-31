30289066d240cbf08e66d82470518c09a223447e
Priyankush Kashyap <priyankushk29@gmail.com>
2023-03-31 17:16:24 +0530

    keys updated

--- src/pages/keys.jsx
+++ src/pages/keys.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import logo from "../assets/animation_640_lfusqhls.gif";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
-const keys = () => {
-    const [show, setShow] = useState(false)
-    return (
-    <div className=" flex flex-col  w-full h-screen  text-white bg-[#242038]">
-      <div className=" flex flex-col justify-around h-[80%] items-center">
-        <div className=" h-[20%] ">
-          <img className=" w-[100%] h-[100%] " src={logo} alt="" />
+const Keys = () => {
+  const [showKeys, setShowKeys] = useState(false);
+
+  return (
+    <div className="flex flex-col w-full h-screen text-white bg-[#242038]">
+      <div className="flex flex-col justify-around h-[80%] items-center">
+        <div className="h-[20%]">
+          <img className="w-[100%] h-[100%]" src={logo} alt="" />
         </div>
         <div>
           <center>
-            <h1>You have successfully </h1>
+            <h1>You have successfully</h1>
             <h1>registered with Proteus!</h1>
             <p>
               Here are your generated public and private keys. Keep track of the
@@ -21,31 +22,30 @@ const keys = () => {
           </center>
         </div>
 
-        <div className=" flex flex-row justify-around w-full ">
-          <div className="  py-2 bg-[#7189FF] rounded-xl px-12 text-center my-3 mx-auto">
+        <div className="flex flex-row justify-around w-full ">
+          <div className="py-2 bg-[#7189FF] rounded-xl px-12 text-center my-3 mx-auto">
             <button>Profile Key</button>
-            {!show&&<AiFillEye
-              onClick={() => {
-                setShow(true);
-              }}
-            //   className={}
-            />}
-            {show&&<AiFillEyeInvisible
-              onClick={() => {
-                setShow(false);
-              }}
-            />}
+            {showKeys ? (
+              <AiFillEyeInvisible onClick={() => setShowKeys(false)} />
+            ) : (
+              <AiFillEye onClick={() => setShowKeys(true)} />
+            )}
           </div>
-          <div className="  py-2 bg-[#7189FF] rounded-xl px-12 text-center my-3 mx-auto">
-            <button>Profile Key</button>
-            <AiFillEyeInvisible />
-            <AiFillEye />
+          <div className="py-2 bg-[#7189FF] rounded-xl px-12 text-center my-3 mx-auto">
+            <button>Transaction Key</button>
+            {showKeys ? (
+              <AiFillEyeInvisible onClick={() => setShowKeys(false)} />
+            ) : (
+              <AiFillEye onClick={() => setShowKeys(true)} />
+            )}
           </div>
-          <div className=" rounded-xl  py-2 bg-[#7189FF]    px-12  text-center my-3 mx-auto">
-            <button>Transaction key</button>
-
-            <AiFillEyeInvisible />
-            <AiFillEye />
+          <div className="rounded-xl py-2 bg-[#7189FF] px-12 text-center my-3 mx-auto">
+            <button>Verification Key</button>
+            {showKeys ? (
+              <AiFillEyeInvisible onClick={() => setShowKeys(false)} />
+            ) : (
+              <AiFillEye onClick={() => setShowKeys(true)} />
+            )}
           </div>
         </div>
       </div>
@@ -53,4 +53,4 @@ const keys = () => {
   );
 };
 
-export default keys;
+export default Keys;
