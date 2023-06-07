ef4aaeeb908d27fed141c611f4b664ff116ee2f0
chandan0134 <chandanma39@gmail.com>
2023-06-08 00:08:48 +0530

    commit

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Profile from "../assets/home icon.png";
 import Profile2 from "../assets/user icon.png";
+import Profile3 from "../assets/Group 27.png"
 
 
 const profile = ()=>{
@@ -16,14 +17,146 @@ const profile = ()=>{
          <div className=" my-10">
             <img className=" w-[80%]   object-contain" src={Profile2} alt=""/>
          </div>
+         <div className=" my-10">
+            <img className=" w-[80%]   object-contain" src={Profile3} alt=""/>
+         </div>
+      </div>
 
-         
-      <div className="w-85vh bg-[#242038]">
+      <div className="flex-1 bg-[#242038]">
+        <div className=" py-5 px-8 flex text-white font-Aquire text-3xl"><h1>PROTEUS</h1></div>
+        
+        <div className=" py-5 px-8 flex  text-white font-Gilroy text-6xl"><h1>Hello Priyankush</h1></div>
+
+        <div className="flex flex-row ">
+          <div className="w-[50%] px-10 text-white font-Gilroy text-1.5xl"><h1>User Details </h1></div>
+          <div className="w-[50%] px-10 text-white font-Gilroy text-1.5xl"><h1>Transaction Details</h1></div>
+        </div>
+
+        <div className=" mx-10 flex border-b-2 border-[#9E378A]"></div>
+
+ 
+          <div className="flex flex-row ">
+          <div className="w-[50%] mx-10 my-10 py-5 h-[100%] px-10 border-radius-10 border-red-700 bg-[#514976]">
+            <div className="flex flex-row">
+            <div className="w-[50%] text-white font-Gilroy"> 
+                 <h1>Transaction Key</h1>
+            </div>
+            <div className="w-[50%] px-10 text-white font-Gilroy"> 
+                 <h1>123456</h1>
+            </div>
+
+            </div>
+            <div  className="flex flex-row">
+            <div className="w-[50%] my-10  text-white font-Gilroy"> 
+                 <h1>Name</h1>
+            </div>
+            <div className="w-[50%] px-10 py-10 text-white font-Gilroy"> 
+                 <h1>123456</h1>
+            </div>
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%]  text-white font-Gilroy"> 
+                 <h1>Contact</h1>
+            </div>
+            <div className="w-[50%] px-10  text-white font-Gilroy"> 
+                 <h1>9957680702</h1>
+
+            </div>
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%] py-10 text-white font-Gilroy"> 
+                 <h1>Gender</h1>
+            </div>
+            <div className="w-[50%] px-10 py-10 text-white font-Gilroy"> 
+                 <h1>Male</h1>
+
+            </div>
+            </div>
+
+              <div className="flex flex-row">
+            <div className="w-[50%]  text-white font-Gilroy"> 
+                 <h1>Age</h1>
+            </div>
+            <div className="w-[50%] px-10  text-white font-Gilroy"> 
+                 <h1>23</h1>
+
+            </div>
+            </div>
+            
+            
+          </div>
+          <div className="w-[50%] mx-10 my-10 py-5 h-[100%] px-10 border-radius-10  border-red-700 bg-[#514976]">
+          <div className="flex flex-row">
+          <div className="w-[50%] text-white font-Gilroy"> 
+                 <h1>Transaction ID</h1>
+            </div>
+            <div className="w-[50%] text-white font-Gilroy"> 
+                 <h1>123456</h1>
+            </div>
+            
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>Time</h1>
+            </div>
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>23:11</h1>
+            </div>
+
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>Balance</h1>
+            </div>
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>500</h1>
+            </div>
+
+            </div>
+            <div className="  flex border-b-2 border-white"></div>
+            <div className="flex flex-row">
+            <div className="w-[50%]  my-2 text-white font-Gilroy"> 
+                 <h1>Transaction ID</h1>
+
+            </div>
+            <div className="w-[50%]  my-2 text-white font-Gilroy"> 
+                 <h1>35373827</h1>
+                 
+            </div>
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>Time</h1>
+            </div>
+            <div className="w-[50%] my-2 text-white font-Gilroy"> 
+                 <h1>13:45</h1>
+            </div>
+          
+            </div>
+            <div className="flex flex-row">
+            <div className="w-[50%]  text-white font-Gilroy"> 
+                 <h1>Balance</h1>
+            </div>
+            <div className="w-[50%]  text-white font-Gilroy"> 
+                 <h1>300</h1>
+            </div>
+
+            </div>
+            <div className=" my-3 flex border-b-2 border-white"></div>
+
+        
+
+          </div>
+
+
+          </div>
+
+        
+
 
       </div>
 
 
-      </div>
     </div>
   );
 };
